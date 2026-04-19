d64225cad9ab83a609c4d6d200a71da6e18b8068
Johan Bäckman <johan.c.backman@gmail.com>
2026-04-19 21:36:06 +0200

    Update section for lcc

--- data/exports/lcc.ts
+++ data/exports/lcc.ts
@@ -7,19 +7,17 @@ export const lcc: Addon = {
   export: {
     name: "LucyCursorCooldowns",
     description: "Shows failed-to-cast spells on the cursor",
-    lastUpdated: "2026-02-06T00:09:40.069Z",
+    lastUpdated: "2026-04-19T19:36:06.617Z",
     downloadUrl: 'https://www.curseforge.com/wow/addons/lucycursorcooldowns',
     media: [
       { type: 'video', url: 'https://i.imgur.com/75b4hZq.mp4' },
       { type: 'image', url: 'https://media.wago.io/screenshots/ARfkAWuDZ/5faeb412ec71dd226ed70bf5.gif' },
       { type: 'image', url: 'https://i.imgur.com/eIGCoZ6.png' },
     ],
-    tags: [category.misc, tag.frames, tag.ui],
+    tags: [category.core, tag.frames, tag.ui],
     setupInstructions: `Nothing required to setup really, just install and use!
 
 Settings: /lcc
-
-Note: Because of api changes it only works out of combat 🥲
 `
   },
 };
